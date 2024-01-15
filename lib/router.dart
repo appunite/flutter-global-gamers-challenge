bf3786e5e6ae1c15ef0f5898a327b789261d805c
JakubKostrzewsky <jk.jakubkostrzewski@gmail.com>
2024-01-15 16:07:25 +0100

    feat: add solar panel scratcher

--- lib/router.dart
+++ lib/router.dart
@@ -1,8 +1,9 @@
-import 'flame_game/game_screen.dart';
+import 'package:endless_runner/challenges/solar_panel_scratcher/solar_panel_scratcher_screen.dart';
 import 'package:flutter/foundation.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 
+import 'flame_game/game_screen.dart';
 import 'level_selection/level_selection_screen.dart';
 import 'level_selection/levels.dart';
 import 'main_menu/main_menu_screen.dart';
@@ -46,6 +47,12 @@ final router = GoRouter(
           path: 'settings',
           builder: (context, state) => const SettingsScreen(
             key: Key('settings'),
+          ),
+        ),
+        GoRoute(
+          path: 'solar-panel-scratcher',
+          builder: (context, state) => const SolarPanelScratcherScreen(
+            key: Key('solar-panel-scratcher'),
           ),
         ),
       ],
