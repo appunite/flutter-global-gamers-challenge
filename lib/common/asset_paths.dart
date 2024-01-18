5931b399777592f413b23a5de374e3f106e956c0
Agnieszka Forajter <agnieszka.forajter@appunite.com>
2024-01-18 12:57:04 +0700

    fix: cleaning

--- lib/common/asset_paths.dart
+++ lib/common/asset_paths.dart
@@ -1,16 +1,16 @@
 class AssetPaths {
-  static const String pipe = 'assets/images/challenges/pipe.png';
-  static const String dumpsterRecycling = 'assets/images/challenges/dumpster_recycling.png';
-  static const String dumpster = 'assets/images/challenges/dumpster.png';
-  static const String tree = 'assets/images/challenges/tree.png';
-  static const String appleCore = 'assets/images/challenges/apple.png';
-  static const String bananaPeel = 'assets/images/challenges/banana.png';
-  static const String diaper = 'assets/images/challenges/diaper.png';
-  static const String glassBottle = 'assets/images/challenges/glass_bottle.png';
-  static const String plasticBottle = 'assets/images/challenges/plastic_bottle.png';
-  static const String cardboard = 'assets/images/challenges/cardboard.png';
-  static const String can = 'assets/images/challenges/can.png';
-  static const String documents = 'assets/images/challenges/documents.png';
-  static const String lightbulb = 'assets/images/challenges/lightbulb.png';
-  static const String fishbones = 'assets/images/challenges/fishbones.png';
+  static const pipe = 'assets/images/challenges/pipe.png';
+  static const dumpsterRecycling = 'assets/images/challenges/dumpster_recycling.png';
+  static const dumpster = 'assets/images/challenges/dumpster.png';
+  static const tree = 'assets/images/challenges/tree.png';
+  static const appleCore = 'assets/images/challenges/apple.png';
+  static const bananaPeel = 'assets/images/challenges/banana.png';
+  static const diaper = 'assets/images/challenges/diaper.png';
+  static const glassBottle = 'assets/images/challenges/glass_bottle.png';
+  static const plasticBottle = 'assets/images/challenges/plastic_bottle.png';
+  static const cardboard = 'assets/images/challenges/cardboard.png';
+  static const can = 'assets/images/challenges/can.png';
+  static const documents = 'assets/images/challenges/documents.png';
+  static const lightbulb = 'assets/images/challenges/lightbulb.png';
+  static const fishbones = 'assets/images/challenges/fishbones.png';
 }
