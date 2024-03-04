699bf0a42e13afbc0cc0a5f1caa133a146d400eb
Agnieszka Forajter <agnieszka.forajter@appunite.com>
2024-03-04 13:58:44 +0000

    fix: add padding

--- lib/challenges/common_widgets/challenge_finish_map_button.dart
+++ lib/challenges/common_widgets/challenge_finish_map_button.dart
@@ -4,6 +4,7 @@ import 'package:better_world/leaderboard/introduction/leaderboard_introduction_d
 import 'package:better_world/main_map/main_map_screen.dart';
 import 'package:better_world/player_progress/entities/challenges_entity.dart';
 import 'package:better_world/player_progress/player_progress_controller.dart';
+import 'package:better_world/style/const_values.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
@@ -15,22 +16,25 @@ class ChallengeFinishMapButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MapButton(
-      onTap: () {
-        final playerProgress = context.read<PlayerProgressController>();
-        playerProgress.loadPlayerData();
+    return Padding(
+      padding: displayAdditionalPadding ? const EdgeInsets.all(8) : EdgeInsets.zero,
+      child: MapButton(
+        onTap: () {
+          final playerProgress = context.read<PlayerProgressController>();
+          playerProgress.loadPlayerData();
 
-        if (playerProgress.challenges.getPlayedChallengesCount() == 0) {
-          NavigationHelper.show(
-            context,
-            const LeaderboardIntroductionDialog(shouldGoToLeaderBoardScreen: true),
-          );
-        } else {
-          Future.delayed(const Duration(milliseconds: 900), () {
-            context.go(MainMapScreen.routePath);
-          });
-        }
-      },
+          if (playerProgress.challenges.getPlayedChallengesCount() == 0) {
+            NavigationHelper.show(
+              context,
+              const LeaderboardIntroductionDialog(shouldGoToLeaderBoardScreen: true),
+            );
+          } else {
+            Future.delayed(const Duration(milliseconds: 900), () {
+              context.go(MainMapScreen.routePath);
+            });
+          }
+        },
+      ),
     );
   }
 }
